2b5b133f2023f59a5bcebd5fbf9bb43c439cda97
Jellybeanci <gokselkucuksahin@gmail.com>
2022-03-30 20:05:02 +0300

    Express.js based Socket.io.

---
+++ ts/socket.io/express/server/server.ts
@@ -0,0 +1,29 @@
+import * as express from "express";
+import {Server, ServerOptions} from "socket.io";
+
+const PORT = 8080;
+
+const serverOptions: Partial<ServerOptions> = {
+    cors: {origin: "*"}
+};
+
+const app = express();
+
+app.get('/greet', ((req, res) => {
+    res.status(200).send("hi");
+}));
+
+const server = app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`));
+
+const io = new Server(server, serverOptions);
+
+io.on("connect", socket => {
+    console.log("a new connection established! socket id:", socket.id);
+    console.log("all connections", io.sockets.adapter.rooms)
+    socket.on("message", (userName, message) => {
+        console.log("a message received:", message);
+        io.emit("message", `<b>${userName}</b> <em>said</em> '${message}'`); // every clients
+        socket.emit("ack", `your message: ${message} received!`); // only the current client
+    });
+});
+
